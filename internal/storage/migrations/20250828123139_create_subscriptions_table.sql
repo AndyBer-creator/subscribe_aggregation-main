41f981f5cce38857e822c3017e1a2761d2461b72
AndyBer-creator <constrictor74@mail.ru>
2025-08-28 17:28:57 +0400

    added migrations

--- internal/storage/migrations/20250828123139_create_subscriptions_table.sql
+++ internal/storage/migrations/20250828123139_create_subscriptions_table.sql
@@ -1,6 +1,9 @@
 -- +goose Up
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE subscriptions (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL,
     service_name VARCHAR(255) NOT NULL,
     price BIGINT NOT NULL,
@@ -10,4 +13,5 @@ CREATE TABLE subscriptions (
 );
 
 -- +goose Down
+
 DROP TABLE subscriptions;
