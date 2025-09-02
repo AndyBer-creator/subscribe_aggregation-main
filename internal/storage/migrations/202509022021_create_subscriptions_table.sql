0eaef3d22af5c70671e1a2e07b58d09e723e02d3
AndyBer-creator <constrictor74@mail.ru>
2025-09-02 20:34:09 +0400

    first commit

---
+++ internal/storage/migrations/202509022021_create_subscriptions_table.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE subscriptions (
+    id SERIAL PRIMARY KEY,
+    service_name VARCHAR(255) NOT NULL,
+    price INTEGER NOT NULL,
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE
+);
+
+-- +goose Down
+DROP TABLE subscriptions;
